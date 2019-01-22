b0706bfe6c60e6d3bfa32d63fb0171a966d66320
Rakotoniaina Andy <andymessorem@gmail.com>
2019-01-22 14:35:05 +0100

    competition creation finished

--- src/Controller/CompetitionsController.php
+++ src/Controller/CompetitionsController.php
@@ -25,38 +25,47 @@ class CompetitionsController extends AppController
     }
 
     public function saveNewComp(){
-        $data = $this->getRequest()->getData();
-        foreach($data as $key=>$dat){
-            if($key != 'file' && $key != 'DateCompet')
-            $data[$key] = htmlspecialchars($data[$key]);
+        if(!$this->request->getSession()->read('isAdmin')){
+            $this->Flash->error('Vous devez être un administrateur pour accéder à cette page.');
+            $this->redirect('/');
         }
-        //dd($data);
-        if($data['file']['type'] != 'image/jpeg' || $data['file']['type'] != 'image/png'){
-            $this->Flash->error('Veuillez choisir une image de type .jpeg ou .png!');
-            $this->redirect($this->referer());
-        }
-        move_uploaded_file($data['file']['tmp_name'],
-            WWW_ROOT.'img/'.strtolower($data['file']['name']));
-        $comp = $this->Competitions
-            ->find()
-            ->where(['NomCompetition' => $data['NomCompetition']])
-            ->first();
-        $data['Image'] = strtolower($data['file']['name']);
-        //dd($data);
-        $tosave = $this->Competitions->newEntity();
-        $tosave->NomCompetition = $data['NomCompetition'];
-        $tosave->Lieu = $data['Lieu'];
-        $tosave->DateCompet = $data['DateCompet'];
-        $tosave->Description = $data['Description'];
-        $tosave->Image = $data['Image'];
-        $tosave->terminee = 0;
-
-        if($comp == null){
-            if(!$this->Competitions->save($tosave)){
-                $this->Flash->error('Il y a eu une erreur lors de la sauvegarde des données.');
+        else{
+            $data = $this->getRequest()->getData();
+            foreach($data as $key=>$dat){
+                if($key != 'file' && $key != 'DateCompet')
+                    $data[$key] = htmlspecialchars($data[$key]);
+            }
+            //dd($data['DateCompet']);
+            if($data['file']['type'] != 'image/jpeg' && $data['file']['type'] != 'image/png'){
+                $this->Flash->error('Veuillez choisir une image de type .jpeg ou .png!');
                 $this->redirect($this->referer());
             }
+            move_uploaded_file($data['file']['tmp_name'],
+                WWW_ROOT.'img/'.strtolower($data['file']['name']));
+            $comp = $this->Competitions
+                ->find()
+                ->where(['NomCompetition' => $data['NomCompetition']])
+                ->first();
+            $data['Image'] = strtolower($data['file']['name']);
+            $data['terminee'] = 0;
+            $tosave = $this->Competitions->newEntity($data);
+
+            if($comp == null){
+                if(!$this->Competitions->save($tosave)){
+                    $this->Flash->error('Il y a eu une erreur lors de la sauvegarde des données.');
+                    $this->redirect($this->referer());
+                }else{
+                    $id = $this->Competitions
+                        ->find()
+                        ->select(['id'])
+                        ->where(['NomCompetition =' => $data['NomCompetition']])
+                        ->first();
+                    $this->redirect(['controller'=>'competitions',
+                            'action'=>'affichedetail', $id->id ]);
+                }
+            }
         }
+
 
     }
 
